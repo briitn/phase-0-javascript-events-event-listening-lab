f7a1fd7311cb5b55b187aecc72b81fd35d314330
princegobah <babynamek@gmail.com>
2021-12-23 08:57:06 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+function addingEventListener() {const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert)
+}
+addingEventListener()
+//const test= document.querySelector('div').addEventListener('click', function(){console.log("ki")})
